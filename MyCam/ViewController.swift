cf82f99fc3163c6ec1aa046039bb64dddd2fae63
Thomas Budiman <thomas.ibudiman@utexas.edu>
2023-07-03 10:04:28 +0700

    Initial Commit

---
+++ MyCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCam
+//
+//  Created by Thomas Ian Budiman on 7/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
